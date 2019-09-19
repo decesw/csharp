31590ccce43c3411b44bf303ae790333189e3c7e
Dece Software <umit.tamer@dece.com.tr>
2019-09-19 18:01:46 +0300

    Add files via upload

---
+++ WebFeedEnumeratorSamples/SampleWebHandler_3_5/App_Code/EventServiceHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+
+
+public class EventEntry
+{
+    public string EventName { get; set; }
+    public string ConetntID { get; set; }
+    public string WorkspaceName { get; set; }
+    public override string ToString()
+    {
+        return string.Format("{0} \t\t{1} \t\t{2}", this.WorkspaceName, this.EventName, this.ConetntID);
+    }
+}
+
+public static class EventServiceHelper
+{
+    public static List<EventEntry> EventList = new List<EventEntry>();
+
+    public static string EventsToString()
+    {
+        StringBuilder all = new StringBuilder();
+        foreach (EventEntry e in EventList)
+            all.AppendLine(e.ToString());
+        return all.ToString();
+    }
+}
